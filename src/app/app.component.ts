c4188e5efb8856d38867ee4a76467f42952c5358
Tejas Shah <shahtejas274@gmail.com>
2017-10-30 10:14:56 -0400

    updated nevigation bar with appropriate methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  insertOrganization(){
+    console.log("hello");
+  }
+
+insertRectangle(){
+    var canvas = <HTMLCanvasElement> document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+    //ctx.fillRect(25, 25, 100, 100);
+    //ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+
+insertTringle(){
+    var canvas = <HTMLCanvasElement> document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+  }
+
+  downLoadImage(){
+    var canvas = <HTMLCanvasElement> document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    window.location.href=image; // it will save locally
+  }
+
+ printCanvas()  
+{  
+
+}
+
+
+insertImage(){
+   
+}
+
+
+
 }
