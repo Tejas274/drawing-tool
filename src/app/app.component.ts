d1941f5716dece835aa4e3db8a0c82930a788aae
Tejas Shah <shahtejas274@gmail.com>
2017-11-06 11:32:48 -0500

    adding new functionality draw ellipse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-
-  insertOrganization(){
+ insertOrganization(){
     console.log("hello");
   }
 
@@ -31,14 +30,14 @@ insertTringle(){
     ctx.fill();
   }
 
-  downLoadImage(){
+downLoadImage(){
     var canvas = <HTMLCanvasElement> document.getElementById("mycanvas");
     var ctx = canvas.getContext("2d");
     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
     window.location.href=image; // it will save locally
   }
 
- printBrush()  
+printBrush()  
 { 
       var el = <HTMLCanvasElement> document.getElementById("mycanvas");
       var ctx = el.getContext('2d');
@@ -96,6 +95,105 @@ el.onmouseup = function() {
 
 }
 
+drawEclipse()
+{
+
+    var el = <HTMLCanvasElement> document.getElementById("mycanvas");
+    var ctx = el.getContext('2d');
+    var isDrawing,x1,y1,isDown=false,x2,y2;
+
+    ctx.translate(0.5, 0.5);  
+
+    el.onmousedown = function(e) {
+      var rect = el.getBoundingClientRect();
+      x1 = e.clientX - rect.left;
+      y1 = e.clientY - rect.top;
+      isDown = true;
+    };
+
+     el.onmouseup = function() {
+         isDown = false;
+    };
+
+    el.onmousemove = function(e) {
+    
+    if (!isDown) return;
+
+       var rect = el.getBoundingClientRect(),
+       x2 = e.clientX - rect.left,
+       y2 = e.clientY - rect.top;
+       
+
+       ctx.clearRect(0, 0, 640, 480);  
+   
+    var radiusX = (x2 - x1) * 0.5,   /// radius for x based on input
+        radiusY = (y2 - y1) * 0.5,   /// radius for y based on input
+        centerX = x1 + radiusX,      /// calc center
+        centerY = y1 + radiusY,
+        step = 0.01,                 /// resolution of ellipse
+        a = step,                    /// counter
+        pi2 = Math.PI * 2 - step;    /// end angle
+
+    /// start a new path
+    ctx.beginPath();
+
+    /// set start point at angle 0
+    ctx.moveTo(centerX + radiusX * Math.cos(0),
+               centerY + radiusY * Math.sin(0));
+
+    /// create the ellipse    
+    for(; a < pi2; a += step) {
+        ctx.lineTo(centerX + radiusX * Math.cos(a),
+                   centerY + radiusY * Math.sin(a));
+    }
+
+    /// close it and stroke it for demo
+    ctx.closePath();
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+       
+
+
+
+    };
+  
+
+}
+
+drawEllipse(x1, y1, x2, y2)   {
+ 
+    var el = <HTMLCanvasElement> document.getElementById("mycanvas");
+    var ctx = el.getContext('2d');
+   
+    var radiusX = (x2 - x1) * 0.5,   /// radius for x based on input
+        radiusY = (y2 - y1) * 0.5,   /// radius for y based on input
+        centerX = x1 + radiusX,      /// calc center
+        centerY = y1 + radiusY,
+        step = 0.01,                 /// resolution of ellipse
+        a = step,                    /// counter
+        pi2 = Math.PI * 2 - step;    /// end angle
+
+    /// start a new path
+    ctx.beginPath();
+
+    /// set start point at angle 0
+    ctx.moveTo(centerX + radiusX * Math.cos(0),
+               centerY + radiusY * Math.sin(0));
+
+    /// create the ellipse    
+    for(; a < pi2; a += step) {
+        ctx.lineTo(centerX + radiusX * Math.cos(a),
+                   centerY + radiusY * Math.sin(a));
+    }
+
+    /// close it and stroke it for demo
+    ctx.closePath();
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+}
+
+
+
 clearCanvas()
 {
    var el = <HTMLCanvasElement> document.getElementById("mycanvas");
@@ -104,7 +202,6 @@ clearCanvas()
    ctx.clearRect(0, 0,  640, 480);
 
 }
-
 
 
 
